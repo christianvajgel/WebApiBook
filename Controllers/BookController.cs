8010304d3fe50cadf388859d67e4b96f083041ee
Christian Vajgel <christian.vajgel@al.infnet.edu.br>
2020-07-21 09:12:00 -0300

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Model;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Book> GetBooks() 
+        {
+            return Ok(Book.GetBooks());
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<Book> Get(int id)
+        {
+            var book = Book.GetBooks().FirstOrDefault(x => x.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public ActionResult<Book> Post(Book book) 
+        {
+            Book.Books.Add(book);
+            return Created("", book);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public ActionResult<Book> Delete(int id) 
+        {
+            var book = Book.GetBooks().FirstOrDefault(x => x.Id == id);
+            if (book == null) 
+            {
+                return NotFound();
+            }
+            Book.Books.Remove(book);
+            return NoContent();
+        }
+    }
+}
---
+++ Model/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Model
+{
+    public class Book
+    {
+        public static List<Book> Books { get; set; }
+
+        public int Id { get; set; }
+        public String Author { get; set; }
+        public String Title { get; set; }
+        public Decimal Price { get; set; }
+
+        static Book() 
+        {
+            Books = new List<Book>() {
+                new Book() { Id = 1, Author = "Erick Evans", Title = "Domain Driven Design", Price = 29.99M },
+                new Book() { Id = 2, Author = "Robert C. Martin", Title = "Code Clean", Price = 19.99M },
+                new Book() { Id = 3, Author = "Martin Fowler", Title = "Refactoring", Price = 59.99M }
+            };
+        }
+
+        public static List<Book> GetBooks() 
+        {
+            return Books;
+        }
+    }
+}
